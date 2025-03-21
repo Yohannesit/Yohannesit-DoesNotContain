642438aeb78c3ccff4813ebe8d0c3b2a046f0a4a
Yohannes Itanna <itannayohannes@gmail.com>
2025-03-21 02:57:01 +0000

    Completed Lab Does Not Contain

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,6 +15,15 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        if (arr == null) {
+            return true;
+        }
+
+        for (int num : arr) {
+            if (num == target) {
+                return false;
+            }
+        }
+        return true;
     }
 }
